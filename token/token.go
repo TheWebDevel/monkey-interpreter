10b011adf644fb0b219d44b550f56c3f86b54aa1
TheWebDevel <sathish@skcript.com>
2020-05-25 21:54:58 +0530

    defined tokens

---
+++ token/token.go
@@ -0,0 +1,38 @@
+package token
+
+// A custom type of type string
+type TokenType string
+
+// A struct that has
+type token struct {
+	Type    TokenType
+	Literal string
+}
+
+// Different token types in the monkey programming language
+// We defined it as constants
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers as Literals
+	IDENT = "INDENT" // add, foobar, x, y...
+	INT   = "INT"    // 12345
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
